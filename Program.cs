ce2453e65cd784647156af118318ad3e8cb16888
Joel Liranzo Rosario <jliranzo@ocabcde.onmicrosoft.com>
2022-01-13 10:19:41 -0400

    app

--- Program.cs
+++ Program.cs
@@ -1,2 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+public class FullLoadApp
+{
+    Random rand = new Random();
+    List<Thread> threads = new List<Thread>();
+
+    public void KillCore()
+    {
+        long num = 0;
+        while (true)
+        {
+            num += rand.Next(100, 1000);
+            if (num > 1000000) { num = 0; }
+        }
+    }
+
+    public void Main()
+    {
+        while (true)
+        {
+            threads.Add(new Thread(new ThreadStart(KillCore)));
+        }
+    }
+}
